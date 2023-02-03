89ad9ed7e5e2c9dc4513d1e232c8d9dfe9676d24
Greg Broseker <gregbroseker95@gmail.com>
2023-02-02 23:26:57 -0500

    show class inheritance and print the date and time

--- test.py
+++ test.py
@@ -1,6 +1,23 @@
-print("\033c")
-print('Hello World')
-print('Desktop remote commit')
-print("I Love Python!")
-print("please pull")
-print("I Love you! :)")
+import datetime
+
+class Person: #parent class
+  def __init__(self, fname, lname):
+    self.first_name = fname
+    self.last_name = lname
+
+#This method is redundant and is done from inherited child method below (identify)
+  #def printName(self):  
+    #print(self.first_name, self.last_name)
+
+class Age(Person): #child class
+  def __init__(self, fname, lname, age):
+    super().__init__(fname, lname)
+    self.yearsAlive = age
+
+  def identify(self):
+    print("Hello, my name is ", self.first_name, self.last_name, "and I am", self.yearsAlive, "years old.")
+
+x = Age("Greg", "Broseker", 28) 
+x.identify()
+user_date = datetime.datetime(2023, 2, 23, 23, 5, 30, 0)
+print("The user date and time is: ", user_date)
